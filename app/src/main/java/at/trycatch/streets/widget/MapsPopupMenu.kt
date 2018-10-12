f5b48fb0f1bf3a927b87f9c287f168ddc202a8ee
Markus Deutsch <markus.deutsch@edu.fh-joanneum.at>
2018-10-12 11:11:00 +0200

    #8: Feedback option.

--- app/src/main/java/at/trycatch/streets/widget/MapsPopupMenu.kt
+++ app/src/main/java/at/trycatch/streets/widget/MapsPopupMenu.kt
@@ -2,28 +2,49 @@ package at.trycatch.streets.widget
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
+import android.support.v7.app.AlertDialog
 import android.view.View
 import android.widget.PopupMenu
 import at.trycatch.streets.R
 import at.trycatch.streets.activity.LegalActivity
 
+
 /**
  * @author Markus Deutsch <markus@moop.at>
  */
-class MapsPopupMenu(val context: Context, val view: View) {
+class MapsPopupMenu(private val context: Context, val view: View) {
 
     private val popupMenu = PopupMenu(context, view)
 
     init {
         popupMenu.inflate(R.menu.menu_main)
         popupMenu.setOnMenuItemClickListener {
-            if (it.itemId == R.id.menu_legal) context.startActivity(Intent(context, LegalActivity::class.java))
+            when (it.itemId) {
+                R.id.menu_legal -> context.startActivity(Intent(context, LegalActivity::class.java))
+                R.id.menu_feedback -> {
+                    AlertDialog.Builder(context)
+                            .setTitle(R.string.feedback_dialog_title)
+                            .setMessage(R.string.feedback_dialog_contents)
+                            .setNegativeButton(R.string.all_cancel, null)
+                            .setPositiveButton(R.string.all_ok) { _, _ ->
+                                startFeedbackIntent()
+                            }
+                            .show()
+                }
+            }
             return@setOnMenuItemClickListener true
         }
     }
 
     fun show() = popupMenu.show()
 
-    fun dismiss() = popupMenu.dismiss()
+    private fun startFeedbackIntent() {
+        val emailIntent = Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                "mailto", "support@moop.at", null))
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Streets Feedback")
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("support@moop.at"))
+        context.startActivity(Intent.createChooser(emailIntent, context.getString(R.string.feedback_dialog_title)))
+    }
 
 }
